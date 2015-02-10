0fb243c2b722f3f4de1b240f056ad65ffc0a723f
Benoit Viguier <benoit+github@wizacha.com>
2015-02-10 22:08:00 +0100

    Flash sequence

--- arduino.cpp
+++ arduino.cpp
@@ -1,16 +1,30 @@
 #include <Arduino.h>
 
-#include "ConsoleAdapter.h"
-#include <iostream>
-
-bveing::ConsoleAdapter arduino(std::cout);
-
 void setup()
 {
-	arduino.pinMode(5, 1);
 }
+
+#define LED_B 1
+#define LED_R 2
+#define FLASH_TIME 80
 
 void loop()
 {
-	arduino.digitalWrite(5, LOW);
+	const unsigned long m 		 = millis() % (32 * FLASH_TIME);
+	const unsigned long sub_beat = m / FLASH_TIME;
+	
+	if( sub_beat % 2) {
+		digitalWrite(LED_B, LOW);
+		digitalWrite(LED_R, LOW);
+		return;
+	}
+	const unsigned long beat = sub_beat / 4;
+	
+	/**
+	 * 0011223344556677
+	 * BB  BB  BB  BBBB
+	 *   RR  RR  RRRRRR
+	 **/
+	digitalWrite(LED_B, (!(beat % 2) || beat == 7) ? HIGH : LOW);
+	digitalWrite(LED_R, (  beat % 2  || beat == 6) ? HIGH : LOW);
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,41 @@
 #include <Arduino.h>
+#include <sys/time.h>
+#include <iostream>
+
+uint8_t states[8] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
+unsigned long millis_start;
+
+unsigned long absolute_millis(void)
+{
+	timeval time;
+	gettimeofday(&time, NULL);
+	return (time.tv_sec * 1000) + (time.tv_usec / 1000);
+}
+
+unsigned long millis(void)
+{
+	return absolute_millis() - millis_start;
+}
+
+void digitalWrite(uint8_t pin, uint8_t val)
+{
+	states[pin] = val;
+}
 
 int main(void)
 {
+	
+	const uint nb_pins = (sizeof(states)/sizeof(*states));
+	millis_start = absolute_millis();
+	
 	setup();
     
 	for (;;) {
 		loop();
+		for(uint i =0; i < nb_pins; ++i) {
+			std::cout<< (states[i] ? 'O' : '.') << " ";
+		}
+		std::cout << millis() << "\r";
 	}
         
 	return 0;
